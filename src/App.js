b7c7e7b2b5c4fae57f4a7a2386d38936a1ee81d1
Mikael Doverhag <doverhag@gmail.com>
2019-05-15 01:36:00 +0000

    Added material ui and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-const App = () => <div>Hello Greenfield</div>;
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
+
+const App = props => (
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <div>Hello Greenfield</div>
+  </MuiThemeProvider>
+);
 
 export default App;
