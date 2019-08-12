a542ad267763c1460723c0808528874eadb907a0
Mikael Doverhag <doverhag@gmail.com>
2019-08-12 13:34:06 +1000

    Changed auth-provider for login endpoint

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Field, Formik, Form } from "formik";
 import Button from "@material-ui/core/Button";
 import FormHelperText from "@material-ui/core/FormHelperText";
+import { Mutation } from "react-apollo";
 
 import ContentDiv from "./lib/ContentDiv";
 import TextField from "./lib/Form/TextField";
 import RootDiv from "./lib/RootDiv";
 
+import { gql } from "apollo-boost";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import * as yup from "yup";
 
-import auth from "../lib/auth";
 import history from "../lib/history";
 
 const styles = (theme: Theme) =>
@@ -23,10 +24,30 @@ const styles = (theme: Theme) =>
     }
   });
 
+const LOGIN = gql`
+  mutation Login($email: String!) {
+    login(email: $email) {
+      email
+      message
+    }
+  }
+`;
+
 interface Props {
   classes: {
     button: string;
   };
+}
+
+interface LoginData {
+  login: {
+    email: string;
+    message: string;
+  };
+}
+
+interface LoginVariables {
+  email: string;
 }
 
 const LoginSchema = yup.object().shape({
@@ -36,44 +57,57 @@ const LoginSchema = yup.object().shape({
     .required("Required")
 });
 
-const Login: React.FC<Props> = props => (
-  <RootDiv>
-    <ContentDiv withPaper>
-      <Formik
-        initialValues={{ email: "" }}
-        validationSchema={LoginSchema}
-        onSubmit={values => {
-          auth.login(values.email).then(() => {
-            history.push("/login/verify");
-          });
-        }}
-      >
-        {({ dirty, isSubmitting, status }) => (
-          <Form noValidate>
-            <Field
-              type="email"
-              name="email"
-              label="Email"
-              component={TextField}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-              className={props.classes.button}
-              disabled={isSubmitting || !dirty}
-            >
-              Sign In
-            </Button>
-            {status && status.error ? (
-              <FormHelperText error={true}>{status.error}</FormHelperText>
-            ) : null}
-          </Form>
-        )}
-      </Formik>
-    </ContentDiv>
-  </RootDiv>
-);
+const Login: React.FC<Props> = props => {
+  const [verifyEmail, setVerifyEmail] = useState();
+  useEffect(() => {
+    if (verifyEmail) {
+      history.push("/login/verify", { email: verifyEmail });
+    }
+  }, [verifyEmail]);
+  return (
+    <RootDiv>
+      <ContentDiv withPaper>
+        <Mutation<LoginData, LoginVariables> mutation={LOGIN}>
+          {(login, { data }) => {
+            if (data) setVerifyEmail(data.login.email);
+            return (
+              <Formik
+                initialValues={{ email: "" }}
+                validationSchema={LoginSchema}
+                onSubmit={({ email }) => login({ variables: { email } })}
+              >
+                {({ dirty, isSubmitting, status }) => (
+                  <Form noValidate>
+                    <Field
+                      type="email"
+                      name="email"
+                      label="Email"
+                      component={TextField}
+                    />
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                      className={props.classes.button}
+                      disabled={isSubmitting || !dirty}
+                    >
+                      Sign In
+                    </Button>
+                    {status && status.error ? (
+                      <FormHelperText error={true}>
+                        {status.error}
+                      </FormHelperText>
+                    ) : null}
+                  </Form>
+                )}
+              </Formik>
+            );
+          }}
+        </Mutation>
+      </ContentDiv>
+    </RootDiv>
+  );
+};
 
 export default withStyles(styles)(Login);
