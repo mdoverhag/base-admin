42c82a423522ebb9bbd8aec831e95b37cb92090e
Mikael Doverhag <doverhag@gmail.com>
2019-08-19 13:16:12 +1000

    Adjusted logout component to just reload page
    The server is then responsible for removing the session and redirecting

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,27 +1,8 @@
-import React, { useEffect } from "react";
-
-import { Link } from "react-router-dom";
-
-import ContentDiv from "./lib/ContentDiv";
-import RootDiv from "./lib/RootDiv";
-
-import { unsetProfile } from "../store/profile/actions";
-import store from "../store";
+import React from "react";
 
 const Logout: React.FC = () => {
-  useEffect(() => {
-    (() => {
-      localStorage.removeItem("accessToken");
-      store.dispatch(unsetProfile());
-    })();
-  });
-  return (
-    <RootDiv>
-      <ContentDiv withPaper>
-        You are logged out: <Link to="/login">Log in</Link>
-      </ContentDiv>
-    </RootDiv>
-  );
+  window.location.reload();
+  return null;
 };
 
 export default Logout;
